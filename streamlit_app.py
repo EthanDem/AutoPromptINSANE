208e6ab2e13e04af84b9c5afac15e4d6e4407ce6
EthanDem <113375436+EthanDem@users.noreply.github.com>
2023-12-15 09:33:47 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,27 @@ import streamlit as st
 import requests
 
 
+st.set_page_config(page_title="INSANE", page_icon=":shushing_face:")
+
+hide_streamlit_style = """
+<style>
+#MainMenu {visibility: hidden;}
+footer {
+    visibility: hidden;
+}
+footer:after {
+    content: 'made by nBrain';
+    visibility: visible;
+    display: block;
+    position: relative;
+    /* background-color: red; */
+    padding: 5px;
+    top: 2px;
+}
+</style>
+"""
+
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 url = "https://test-auto-prompt-select.icecube1513.repl.co/request"
 
